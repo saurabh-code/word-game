55887552db7d8d12bc2a7691d48aa28162904eed
saurabh-code <saurabh.kumar.code@gmail.com>
2018-08-09 16:23:50 +0530

    letter similar count

--- src/main/java/com/bootcamp/word_game/WordUtils.java
+++ src/main/java/com/bootcamp/word_game/WordUtils.java
@@ -1,6 +1,27 @@
 package com.bootcamp.word_game;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class WordUtils {
 
+	public static int countSimilarity(String fixedWord, String guessedWord) {
+		List<String> fixedLetters = new ArrayList<>(Arrays.asList(fixedWord.split("")));
+		List<String> guessedLetters = new ArrayList<>(Arrays.asList(guessedWord.split("")));
+		
+		int count = 0;
+		for (String s: guessedLetters) {
+			if (fixedLetters.contains(s)) {
+				count++;
+				fixedLetters.remove(fixedLetters.indexOf(s));
+			}
+		}
+		return count;
+	}
+	
+	public static boolean isWin(String fixedWord, String guessedWord) {
+		return fixedWord.equals(guessedWord);
+	}
 	
 }
--- src/test/java/com/bootcamp/word_game/WordUtilsTest.java
+++ src/test/java/com/bootcamp/word_game/WordUtilsTest.java
@@ -1,5 +1,6 @@
 package com.bootcamp.word_game;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -7,8 +8,13 @@ import org.junit.Test;
 public class WordUtilsTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSimple() {
+		assertThat(WordUtils.countSimilarity("A", "A"), is(1));
+		assertThat(WordUtils.countSimilarity("B", "A"), is(0));
+		assertThat(WordUtils.countSimilarity("AB", "AB"), is(2));
+		assertThat(WordUtils.countSimilarity("ABC", "CAZ"), is(2));
+		assertThat(WordUtils.countSimilarity("AABC", "ABC"), is(3));
+		assertThat(WordUtils.countSimilarity("BAC", "DZA"), is(1));
 	}
 
 }
